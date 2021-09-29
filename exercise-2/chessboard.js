a73f1c95384c094007db0cc730ab1b901c8d2f47
ravish0007 <ravish.nfs@gmail.com>
2021-09-29 03:25:44 +0000

    Adds exercise-2, chessboard

---
+++ exercise-2/chessboard.js
@@ -0,0 +1,25 @@
+let grid_size = 64;
+
+let pattern_x = "";
+let pattern_y = "";
+
+
+for ( let i = 0; i < grid_size; i++) {
+	if (i%2)  {
+		pattern_x += "#" ;
+		pattern_y += " ";
+	}
+	else {
+		pattern_x += " ";
+		pattern_y += "#";
+	}
+}
+
+for ( let i = 0; i < grid_size; i += 2) {
+	console.log(pattern_x);
+	console.log(pattern_y);
+}
+	
+
+
+
