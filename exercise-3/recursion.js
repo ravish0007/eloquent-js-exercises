20221c60019aad193fd4388dfd63eaa30ba1493e
ravish0007 <ravish.nfs@gmail.com>
2021-10-02 15:09:48 +0000

    Add recursion.js

---
+++ exercise-3/recursion.js
@@ -0,0 +1,15 @@
+function isEven(num) {
+
+    if (num < 0) num *= -1;
+
+    if (num == 0) return true;
+    else if (num == 1) return false;
+    else return isEven(num - 2);
+}
+
+
+console.log(isEven(50));
+
+console.log(isEven(75));
+
+console.log(isEven(-1));
