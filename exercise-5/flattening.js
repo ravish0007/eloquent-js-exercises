7e7ed6543e5be50789dfdc91be218cf8a9e5b93f
ravish0007 <ravish.nfs@gmail.com>
2021-10-03 09:59:46 +0000

    modify flattening.js

--- exercise-5/flattening.js
+++ exercise-5/flattening.js
@@ -1,8 +1,8 @@
 
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
-result = arrays.reduce( (a, b) => a.concat(b));
+let result = arrays.reduce( (a, b) => a.concat(b));
 
-console.log(results);
+console.log(result);
 
 
